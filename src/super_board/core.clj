5a10e20df0afeb1c5c35ce8d601fde6f7e11ed00
Erik <erik.svedang@gmail.com>
2013-06-23 22:49:28 +0200

    better formatting, can look at a simple game state

--- src/super_board/core.clj
+++ src/super_board/core.clj
@@ -1,2 +1,25 @@
-(ns super-board.core)
+(ns super-board.core
+  (require [super-board.data-to-html :as data-to-html]))
+
+(def start-state {:players ["Kalle" "Hobbe"]
+                  :zones [:hand :deck :table]
+                  :turn 0})
+
+(defn tick [game-state]
+  (update-in game-state [:turn] inc))
+
+(defn game-reduce [start-state step-fn turns]
+  (loop [result []
+         state start-state
+         turns-left turns]
+    (if (pos? turns-left)
+      (let [new-state (step-fn state)]
+        (recur (conj result new-state)
+               new-state
+               (dec turns-left)))
+      result)))
+
+(def game-states (game-reduce start-state tick 3))
+
+(spit "html/output.html" (data-to-html/convert game-states))
 
--- src/super_board/data_to_html.clj
+++ src/super_board/data_to_html.clj
@@ -3,22 +3,24 @@
   (require [hiccup.page :as p]))
 
 
-(def struct-1 [{:name "Erik" :age 26 :siblings ["anna" "jonas" "klara"]}
-               {:name "Marie" :age 27}])
-
-(def struct-2 ["erik" "anna" "jonas" "klara"])
+(def struct-1 [{20 100 :name "Erik" :age 26.78 :siblings #{"anna" "jonas" "klara"} :letters (set "abcdefghijklmnopqrstuvwxyz")}
+               {:name "Marie" :age 27 :smart true :cities ["Stockholm" "Gothenburg"]}
+               {:name "Tobbe" :age 32 :instruments [:guitar :drum]}])
 
 (defn visit-vector [index element]
   (list [:div.index (str index)]
         (visit element)))
 
 (defn visit-map-pair [[k v]]
-  (list [:div.key (name k)]
+  (list [:div.key (if (keyword? k) (name k) (str k))]
         (visit v)))
 
 (defn visit [node]
   (cond (vector? node) [:div.vector (map visit-vector (range) node)]
+        (set? node) [:div.set (map visit node)]
         (map? node) [:div.map (map visit-map-pair node)]
+        (string? node) [:div.string (str \" node \")]
+        (keyword? node) [:div.keyword (str node)]
         :else [:div.type (str node)]))
 
 (defn convert [input]
@@ -26,6 +28,6 @@
    (p/include-css "style.css")
    (p/html5 [:body (visit input)])))
 
-(spit "html/output.html" (convert struct-1))
+;(spit "html/output.html" (convert struct-1))
 
 
