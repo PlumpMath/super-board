5473a807a566ad5cd1e2c01f8b8ed9fa70469ad0
Erik <erik.svedang@gmail.com>
2013-06-23 23:33:54 +0200

    removed output

--- src/super_board/core.clj
+++ src/super_board/core.clj
@@ -2,16 +2,25 @@
   (require [super-board.data-to-html :as data-to-html]
            [super-board.structure :as structure]))
 
-(def start-state {:players ["Pigge" "Kalle" "Hobbe"]
+(defn create-player [nick]
+  {:nick nick
+   :score 0})
+
+(def start-state {:players [(create-player "Pigge")
+                            (create-player "Kalle")
+                            (create-player "Hobbe")]
                   :zones [:hand :deck :table]
                   :turn "Pigge"})
 
-(defn switch-player [current-player players]
-  (let [other (structure/next-player-map players)]
+(defn switch-player [current-player player-nicks]
+  (let [other (structure/next-player-map player-nicks)]
     (other current-player)))
 
+(defn player-nicks [game-state]
+  (map :nick (:players game-state)))
+
 (defn tick [game-state]
-  (update-in game-state [:turn] switch-player (:players game-state)))
+  (update-in game-state [:turn] switch-player (player-nicks game-state)))
 
 (def game-states (structure/game-reduce start-state tick 10))
 
